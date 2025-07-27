6c4c1624cb1d7e8b8fd7faee08849cbcf09a414c
Akhil-M-Patel <akhilmaheshpatel@gmail.com>
2025-07-27 13:38:29 +0530

    Add files via upload

---
+++ Task-2.py
@@ -0,0 +1,12 @@
+from math import *
+
+n = int(input("Enter a number:"))
+
+sq_root = sqrt(n)
+natural_log = log(n)
+Sine=sin(n)
+
+print(f"Square root:{sq_root}")
+print(f"Logarithm:{natural_log}")
+print(f"Sine:{Sine}")
+
